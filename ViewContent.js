7a21ef50c9690a1571893da7e076a225a4d38c09
RaphaDevsearch <mizaraphaelpro@gmail.com>
2026-06-04 09:05:29 +0530

    understand layour

--- ViewContent.js
+++ ViewContent.js
@@ -2,14 +2,8 @@ import { CreateElement } from "./Utils.js";
 export const ViewContent = () => {
   return CreateElement("section", {
     className: "view-content",
-    children: [
-      CreateElement("h2", {
-        textContent: "Dashboard"
-      }),
-
-      CreateElement("p", {
-        textContent: "Main content goes here."
-      })
-    ]
+    children: Array.from({ length: 300 }, (_, i) => CreateElement("p", {
+        textContent: `Main content goes here. (${i + 1})`
+      }))
   });
 };
